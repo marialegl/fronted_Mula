5524ec8dd5c577ca2823d716052384b296fcc032
Julio Luque <julioluque@MacBook-Air-de-Julio.local>
2024-11-21 08:23:11 -0500

    Update splash

---
+++ iosApp/iosApp/AppDelegate.swift
@@ -0,0 +1,28 @@
+import UIKit
+import shared
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+
+        startSplash {
+            self.showMainScreen()
+        }
+
+        return true
+    }
+
+    // Función para mostrar la pantalla principal
+    private func showMainScreen() {
+        let mainViewController = MainViewController() // Cambia esto al controlador principal de tu app
+        window?.rootViewController = mainViewController
+        window?.makeKeyAndVisible()
+    }
+}
